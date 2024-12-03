8eea362025714537a64a2120dfab7fb924d60f5c
« Jean »-Baptiste <jeanbaptistecornu@gmail.com>
2024-12-03 13:40:57 +0100

    day 3 is regexp day :)

--- exo2.py
+++ exo2.py
@@ -1,24 +1,19 @@
-fichier = open('./data/inputs.txt')
-
-col1 = []
-col2 = []
-for ligne in fichier:
-    col1.append(ligne.strip().split()[0])
-    col2.append(ligne.strip().split()[1])
+import re
 
 
-print(len(col2))
+pattern = r"mul\(\d+,\d+\)"
+pattern_dont = r"don't\(\)"
+pattern_do = r"do\(\)"
 result = 0
 
+with open('./data/inputs.txt') as fichier:
+    occurence = []
+    for ligne in fichier:
+        print("je passe là")
+        print(return item. for item in re.findall(pattern_dont,ligne))
+       
 
-print('col1', len(col1))
-print('col2 ', len(col2))
-occurence = 0
-for item in col1:
-    occurence += int(item)*int(col2.count(item))
+for item in occurence:
+    tab = re.findall(r'-?\d+\.?\d*',item)
+    result += int(tab[0])*int(tab[1])
     
-
-print(occurence)
-
-
-
--- program.py
+++ program.py
@@ -1,26 +1,17 @@
-fichier = open('./data/inputs.txt')
-
-col1 = []
-col2 = []
-for ligne in fichier:
-    col1.append(ligne.strip().split()[0])
-    col2.append(ligne.strip().split()[1])
+import re
 
 
-print(len(col2))
+pattern = r"mul\(\d+,\d+\)"
 result = 0
+with open('./data/inputs.txt') as fichier:
+  
+    occurence = []
+    for ligne in fichier:
+        occurence.extend(re.findall(pattern,ligne))
 
-
-print('col1', len(col1))
-print('col2 ', len(col2))
-tour = 0
-while len(col1)>0 and len(col2)>0:
-    result += abs(int(min(col1))  - int(min(col2)))
-    print('diff',abs(int(min(col1))  - int(min(col2))))
-    print('result',result)
-    col1.remove(min(col1))
-    col2.remove(min(col2))
-
-
+for item in occurence:
+    tab = re.findall(r'-?\d+\.?\d*',item)
+    result += int(tab[0])*int(tab[1])
+    
 
 print(result)
